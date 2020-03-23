16bfb7c41a31be7f018ef60e720bd7770e3e2427
Cathy Ding <Cathy@Cathy-DingdeMacBook-5.local>
2020-03-23 15:10:09 +0800

    Comment out OCR file

--- revision_application/ocr.py
+++ revision_application/ocr.py
@@ -1,18 +1,18 @@
-from PIL import Image
-import pytesseract
-import pdftotext
-import io
-from wand.image import Image as wi
-
-
-def pdf_to_text(f):
-    with open(f, "rb") as f:
-        pdf = pdftotext.PDF(f)
-    # for page in pdf:
-    #     print(page)
-    print("\n\n".join(pdf))
-
-
-def img_to_text(f):
-    # Simple image to string
-    print(pytesseract.image_to_string(Image.open(f)))
+# from PIL import Image
+# import pytesseract
+# import pdftotext
+# import io
+# from wand.image import Image as wi
+#
+#
+# def pdf_to_text(f):
+#     with open(f, "rb") as f:
+#         pdf = pdftotext.PDF(f)
+#     # for page in pdf:
+#     #     print(page)
+#     print("\n\n".join(pdf))
+#
+#
+# def img_to_text(f):
+#     # Simple image to string
+#     print(pytesseract.image_to_string(Image.open(f)))
